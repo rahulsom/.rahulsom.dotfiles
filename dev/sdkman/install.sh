07391272f293fb43abcf5593e1c3c994fc2a6c4f
Rahul Somasunderam <rahul.som@gmail.com>
2020-01-24 17:32:08 -0800

    Install groovy and visualvm

--- dev/sdkman/install.sh
+++ dev/sdkman/install.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 #SDK man and java
-if [ ! -f $HOME/.sdkman/bin/sdkman-init.sh ]; then
-    curl -s "https://get.sdkman.io" | bash
+if [ ! -f "$HOME/.sdkman/bin/sdkman-init.sh" ]; then
+  curl -s "https://get.sdkman.io" | bash
 fi
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 installJava() {
-    VERSION=$(sdk ls java | grep " $1 " | cut -d \| -f 6 | grep $2 | head -1 | xargs echo)
-    INSTALLED=$(sdk ls java | grep "$VERSION" | grep -c installed)
-    if [ "$INSTALLED" = 0 ]; then
-        sdk install java "$VERSION"
-    fi
+  VERSION=$(sdk ls java | grep " $1 " | cut -d \| -f 6 | grep $2 | head -1 | xargs echo)
+  INSTALLED=$(sdk ls java | grep "$VERSION" | grep -c installed)
+  if [ "$INSTALLED" = 0 ]; then
+    sdk install java "$VERSION"
+  fi
+}
+
+installLatest() {
+  VERSION=$(sdk ls $1 | xargs -n 1 echo | grep -E '^[0-9.]+$' | sort --version-sort -r | head -1)
+  if [ ! -d "$HOME/.sdkman/candidates/$1/$VERSION" ]; then
+    sdk install "$1" "$VERSION"
+  fi
 }
 
 installJava zulu 8.0.
 installJava zulu 11.0.
+
+installLatest groovy
+installLatest visualvm
