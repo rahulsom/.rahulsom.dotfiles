47aa449200cd2528a53cb833c9fa20c373f112d1
Rahul Somasunderam <rahul.som@gmail.com>
2021-09-26 16:28:30 -0700

    Separate declaration and assignment when evaluating expressions

--- dev/gradle/functions.zsh
+++ dev/gradle/functions.zsh
@@ -1,12 +1,17 @@
 gradle_prepare_computer_for_release() {
   # Say hello to 1password
-  eval $(op signin my)
+  eval "$(op signin my)"
 
   # Get the good stuff from 1password
-  export ORG_GRADLE_PROJECT_sonatypeUsername=$(op get item issues.sonatype.org | jq -r '.details.fields[] | select(.name == "username") | .value')
-  export ORG_GRADLE_PROJECT_sonatypePassword=$(op get item issues.sonatype.org | jq -r '.details.fields[] | select(.name == "password") | .value')
-  export ORG_GRADLE_PROJECT_signingKey=$(op get item gpg.key | jq -r '.details.notesPlain')
-  export ORG_GRADLE_PROJECT_signingPassword=$(op get item gpg.key | jq -r '.details.fields[] | select(.name == "password") | .value')
+  ORG_GRADLE_PROJECT_sonatypeUsername=$(op get item issues.sonatype.org | jq -r '.details.fields[] | select(.name == "username") | .value')
+  ORG_GRADLE_PROJECT_sonatypePassword=$(op get item issues.sonatype.org | jq -r '.details.fields[] | select(.name == "password") | .value')
+  ORG_GRADLE_PROJECT_signingKey=$(op get item gpg.key | jq -r '.details.notesPlain')
+  ORG_GRADLE_PROJECT_signingPassword=$(op get item gpg.key | jq -r '.details.fields[] | select(.name == "password") | .value')
+
+  export ORG_GRADLE_PROJECT_sonatypeUsername
+  export ORG_GRADLE_PROJECT_sonatypePassword
+  export ORG_GRADLE_PROJECT_signingKey
+  export ORG_GRADLE_PROJECT_signingPassword
 }
 
 gradle_setup_github_secrets() {
