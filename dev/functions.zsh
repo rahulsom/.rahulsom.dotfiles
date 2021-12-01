a745362ed8cdb8b25f511e8701637da8317ce785
Rahul Somasunderam <rsomasunderam@netflix.com>
2021-12-01 10:52:07 -0800

    Add function to generate random password

---
+++ dev/functions.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+function generatePassword() {
+  local LENGTH=${1:-9}
+  local SETS=${2:-aA0}
+  groovy -e "
+    def generator = { String alphabet, int n ->
+      new Random().with {
+        (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+      }
+    }
+
+    def chars = ('a'..'a') - 'a'
+    if ('$SETS'.contains('a')) { chars += ('a'..'z') }
+    if ('$SETS'.contains('A')) { chars += ('A'..'Z') }
+    if ('$SETS'.contains('0')) { chars += ('0'..'9') }
+
+    def charset = chars.join()
+    if ('$SETS'.contains('.')) { charset += '!@#$%^&*-+=.?<>,/;~' }
+
+    println generator(charset, $LENGTH )
+  "
+}
